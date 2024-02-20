b405099885caa3ab0f2803dd8b85721c558a6e0e
David <obododavidsop@gmail.com>
2024-02-20 14:53:34 +0100

    added task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Flask web application - List of states"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def list_states():
+    """List all states"""
+    from models.state import State
+    states = storage.all(cls=State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
