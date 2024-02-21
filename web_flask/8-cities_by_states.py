2bd483d500ac55432171b08b2fa051eeb03ad000
David <obododavidsop@gmail.com>
2024-02-21 03:45:28 +0100

    added 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Flask web application - List of states"""
+from flask import Flask, render_template
+from models import storage, storage_type
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/cities_by_states")
+def list_cities_by_states():
+    """List all cities by states"""
+    states = storage.all(cls=State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
